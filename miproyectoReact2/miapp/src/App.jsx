40a2b7d5da593dc45a59acd31c317a8bb9d07e66
Agustin Donalisio <donalisioagustin@gmail.com>
2022-11-11 15:43:19 -0300

    Aplicando React.Fragment
    una forma para tener solo 1 etiqueta padre y que no sea un div

--- miproyectoReact2/miapp/src/App.jsx
+++ miproyectoReact2/miapp/src/App.jsx
@@ -1,34 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+//importo esto para no tener la etiqueta padre molestando
+import React from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
+    //aplico react.fragment para no utilizar la etiqueta padre
+    <React.Fragment>
+
+      <div className="App">
+        hola bro
+      </div>
+
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        div 2
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+
+    </React.Fragment>
   )
+
 }
 
 export default App
