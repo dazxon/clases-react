be54d91f44d82ef29866fa6f2b1185ce4d85d8e8
Agustin Donalisio <donalisioagustin@gmail.com>
2022-11-14 16:37:34 -0300

    Clase 3 de react

---
+++ nuevo/miapp/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
+
+function App() {
+
+  const [showText,setShowText] = useState('hola mundo')
+  
+  return (
+    <div className='App'>
+
+    </div>
+
+
+  );
+}
+
+export default App;
