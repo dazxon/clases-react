60cb2d2601539abe6177ca7538a3090432e76233
Agustin Donalisio <donalisioagustin@gmail.com>
2022-11-16 17:19:06 -0300

    Convertidor dolar a pesos
    Simple convertidor de dolar/peso peso/dolar, muy copado, me gusto, facil, elegante

--- probando/probando/src/components/convertidor/index.jsx
+++ probando/probando/src/components/convertidor/index.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import { useState } from 'react';
 import './Convertidor.css'
 
 
 const Convertidor = () => {
 
-    
+    const[pesos,setPesos] = useState(0)
 
+    const[dolares,setDolares] = useState(0)
 
+    const misPesos = () => {
+        
+    }
+
+    const misDolares = () => {
+        
+    }
 
     return(
         
@@ -14,10 +23,16 @@ const Convertidor = () => {
             <h2>Convertidor de pesos a dolares</h2>
 
             <label htmlFor="">Pesos</label>
-            <input type="number" />
+            <input type="number" value={pesos} onChange={(x)=>{
+                setPesos(x.target.value)
+                setDolares((x.target.value/300).toFixed(2))
+                }}/>
 
             <label htmlFor="">Dolares</label>
-            <input type="number" />
+            <input type="number" value={dolares} onChange={(x)=>{
+                setDolares(x.target.value)
+                setPesos((x.target.value*300).toFixed(2))
+                }}/>
 
             <input type="submit" value={"Enviar"} />
 
