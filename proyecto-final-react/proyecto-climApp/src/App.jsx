4fa6fafa08c5b7aa6e56e5f15f86702dec23711d
Agustin Donalisio <donalisioagustin@gmail.com>
2022-11-30 02:31:07 -0300

    Chau chau

--- proyecto-final-react/proyecto-climApp/src/App.jsx
+++ proyecto-final-react/proyecto-climApp/src/App.jsx
@@ -1,17 +1,19 @@
 import { useState } from 'react'
 import './App.css'
+import { Header } from "./components/Header/Header";
+import axios from "axios"
+import { Buscador } from './components/Buscador/Buscador'
 
 function App() {
-  
 
   return (
-    <div className="App">
 
+    <div className='container'>
 
-
-
-
+      <Buscador/>
+      
     </div>
+
   )
 }
 
---
+++ proyecto-final-react/proyecto-climApp/src/components/Buscador/Buscador.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Buscador = () => {
+    return(
+        <AsyncPaginate
+
+        
+
+
+        />
+    )
+}
+
+export { Buscador }
